0a8f039070020cc2d332a03878e10cbaa19a4918
Artem Konusheusky <konusevskiart@gmail.com>
2024-01-10 20:41:14 +0300

    feat: add burger menu and use-match-media hook

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -4,6 +4,9 @@ import { useTranslation } from 'react-i18next'
 import { type FC } from 'react'
 import { v4 as uuid } from 'uuid'
 import { NavLink } from 'react-router-dom'
+import { RouterPaths } from 'shared/RouterPaths'
+import { BurgerMenu } from 'widgets/BurgerMenu'
+import { useMatchMedia } from 'shared/hooks/useMatchMedia'
 
 interface NavbarProps {
   className?: string
@@ -11,26 +14,29 @@ interface NavbarProps {
 
 export const Navbar: FC = ({ className }: NavbarProps) => {
   const { t } = useTranslation()
+  const { isMobile } = useMatchMedia()
 
   const links = [
-    { id: uuid(), path: '/', title: t('navigation_main') },
-    { id: uuid(), path: '/personal-cabinet', title: t('personalCabinet') },
-    { id: uuid(), path: '/about', title: t('navigation_about') }
+    { id: uuid(), path: RouterPaths.homePage, title: t('navigation_main') },
+    { id: uuid(), path: RouterPaths.personalCabinetPage, title: t('personalCabinet') },
+    { id: uuid(), path: RouterPaths.aboutPage, title: t('navigation_about') }
   ]
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <nav className={cls.links}>
-        {links.map(({ id, path, title }) => (
-          <NavLink
-            to={path}
-            key={id}
-            className={({ isActive }) => isActive ? classNames(cls.navLink, {}, [cls.active]) : cls.navLink}
-          >
-            {title}
-          </NavLink>
-        )
-        )}
-      </nav>
+        {isMobile
+          ? <BurgerMenu links={links}/>
+          : <nav className={cls.links}>
+              {links.map(({ id, path, title }) => (
+                <NavLink
+                  to={path}
+                  key={id}
+                  className={({ isActive }) => isActive ? classNames(cls.navLink, {}, [cls.active]) : cls.navLink}
+                >
+                  {title}
+                </NavLink>
+              ))}
+            </nav>
+      }
     </div>
   )
 }
