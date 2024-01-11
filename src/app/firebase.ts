f1659db0d22cf157f572eb195ee4f4aed66f329d
Artem Konusheusky <konusevskiart@gmail.com>
2024-01-11 21:49:11 +0300

    test: add REACT_APP_ in front of all variables

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -8,12 +8,12 @@ console.log(process.env.AUTH_DOMAIN)
 console.log('process.env', process.env)
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGE_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
